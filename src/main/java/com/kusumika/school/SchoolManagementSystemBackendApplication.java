f241dee27633cf7a036240cab45a92406c0d04af
nagrohan726 <nag.rohan726@gmail.com>
2023-02-27 08:18:44 +0530

    Base spring boot project

---
+++ src/main/java/com/kusumika/school/SchoolManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kusumika.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementSystemBackendApplication.class, args);
+	}
+
+}
